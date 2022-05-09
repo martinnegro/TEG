2669f19a7272194ad1a2ee54b58506c055b9d4a6
Martin Negro <mnegro91@gmail.com>
2022-05-09 17:10:29 -0300

    env config

--- teg-server/src/db/index.ts
+++ teg-server/src/db/index.ts
@@ -8,6 +8,8 @@ import Game from './models/Game.model';
 import Player from './models/Player.model';
 import Status from './models/Status.model';
 import User from './models/User.model';
+const dotenv = require('dotenv');
+dotenv.config()
 
 const { DATABASE_URL } = process.env
 
