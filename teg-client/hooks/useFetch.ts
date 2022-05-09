12c0ba53e6e7dad20d4acbef943a1e3883a325b7
Martin Negro <mnegro91@gmail.com>
2022-05-09 01:13:59 -0300

    useFetch changed to be a trigger

--- teg-client/hooks/useFetch.ts
+++ teg-client/hooks/useFetch.ts
@@ -5,29 +5,24 @@ export type FetchStatus = 'loading'|'error'|'ok'
 
 const useFetch = <Type>(endpoint: string  | null): [ Type, FetchStatus, Error, Dispatch<SetStateAction<string>> ] => {
     const [ status, setStatus ] = useState<FetchStatus>('loading')
-    const [ url, setUrl ] = useState<string | null>(endpoint)
     const [ data, setData ] = useState<Type | null>(null);
     const [ error, setError ] = useState<Error | null>(null);
 
-    
+    const doFetch = (endpoint: string) => {
+        axios.get(endpoint)
+        .then(({ data }) => {
+            setStatus('ok')
+            setError(null)
+            setData(data)
+        })
+        .catch((err: Error) => {
+            setStatus('error')
+            setData(null)
+            setError(err)        
+        });
+    }    
 
-    useEffect(() => {
-      if(!url) return null
-      axios.get(url)
-      .then(({ data }) => {
-          setStatus('ok')
-          setError(null)
-          setData(data)
-      })
-      .catch((err: Error) => {
-          setStatus('error')
-          setData(null)
-          setError(err)        
-      });
-    },[url]);
-    
-
-    return [ data, status, error, setUrl ]
+    return [ data, status, error, doFetch ]
 };
 
 export default useFetch;
