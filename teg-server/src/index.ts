9b846ae7db61a9c2916e1039f3d837f74fc775e0
Martin Negro <mnegro91@gmail.com>
2022-05-12 13:30:44 -0300

    Loading DB

--- teg-server/src/index.ts
+++ teg-server/src/index.ts
@@ -2,16 +2,16 @@ import http from 'http';
 import app from './app';
 import { dateGen } from './utils/dateGen';
 import Models, { conn } from './db';
-// import preloadDb from './db/preloadDb/preloadDb';
+import preloadDb from './db/preloadDb/preloadDb';
 
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 4000;
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
     console.log(dateGen() + ` ---> Connected to database...`)
     const { Color, Status, Continent, Country, BorderingCountries } = Models
-    // preloadDb(Color, Status, Continent, Country, BorderingCountries)
+    preloadDb(Color, Status, Continent, Country, BorderingCountries)
     server.listen(PORT, () => {
         console.log(dateGen() + ` ---> Server listening in port ${PORT}...`)
     });
